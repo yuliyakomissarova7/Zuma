0b552a2e10c16d586ed74b182d577a16d7b55e9f
yuliyakomissarova7 <yliuakomissarova@gmail.com>
2021-12-30 11:17:10 +0500

    Add assets

---
+++ modules/sprite_manager/skull.py
@@ -0,0 +1,23 @@
+import pygame
+
+from modules.colors import *
+from modules.parameters import *
+from modules.score_manager import ScoreManager
+
+
+class Skull(pygame.sprite.Sprite):
+    def __init__(self, path, balls, score_manager):
+        pygame.sprite.Sprite.__init__(self)
+        self.skull = pygame.transform.smoothscale(pygame.image.load(SKULL), (80, 80))
+        self.score_manager = score_manager
+        self.skull.set_colorkey(BLACK)
+        self.balls = balls
+        self.rect = self.skull.get_rect(center=path.points[-1])
+
+    def update(self):
+        for ball in self.balls:
+            if self.rect.colliderect(ball.rect):
+                self.score_manager.game_lose()
+
+    def draw_sprite(self, display):
+        display.blit(self.skull, (self.rect.x, self.rect.y))
