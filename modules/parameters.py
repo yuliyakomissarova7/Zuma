3400497300b0d0da20d43b718fee1d4859b8beeb
yuliyakomissarova7 <yliuakomissarova@gmail.com>
2021-12-30 11:19:44 +0500

    Add parameters and colors

---
+++ modules/parameters.py
@@ -0,0 +1,11 @@
+WIDTH = 1024
+HEIGHT = 768
+DISPLAY_CENTER = (WIDTH/2, HEIGHT/2)
+FONT = 'modules/assets/fonts/Tigerious.otf'
+FROG = 'modules/assets/images/frog.png'
+SKULL = 'modules/assets/images/skull.png'
+LIFE = 'modules/assets/images/live.png'
+BOMB = 'modules/assets/images/bomb.png'
+ARROW = 'modules/assets/images/arrow.png'
+FONT_SIZE = 20
+BALL_DIAMETER = 40
